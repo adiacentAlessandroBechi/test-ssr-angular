d0220e562dd7ca392951bc2c1ed367e21b9faa17
alessandro.bechi <a.bechi@vargroup.it>
2025-07-15 14:40:38 +0200

    add ssr

---
+++ src/app/app.routes.server.ts
@@ -0,0 +1,25 @@
+import { RenderMode, ServerRoute } from '@angular/ssr';
+
+export const serverRoutes: ServerRoute[] = [
+  {
+      path: '',
+      renderMode: RenderMode.Client,
+  },
+  
+  {
+      path: 'home',
+      renderMode: RenderMode.Client
+  },
+  {
+      path: 'client',
+      renderMode: RenderMode.Client,
+  },
+  {
+      path: 'static',
+      renderMode: RenderMode.Prerender,
+  },
+  {
+      path: 'ssr',
+      renderMode: RenderMode.Server
+  }
+];
